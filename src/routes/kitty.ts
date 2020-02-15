f681fa99a46eeb829f50757649c9df2cbcfebf6e
Paul Gadi <paulgadi@gmail.com>
2020-02-15 10:03:09 -0700

    Update kitty.ts

--- src/routes/kitty.ts
+++ src/routes/kitty.ts
@@ -1,21 +1,32 @@
 export default (app) => {
 
+  // attack: Number(this.kitty.tokenId) % 102, -> should return a higher number
+  // element: Number(this.kitty.tokenId) % 5, -> should return 0 - 4
+  // speed: Number(this.kitty.tokenId) % 42, -> should return a middle number
+  // crit: Number(this.kitty.tokenId) % 12 -> should return a low number
+
+  function getKittyStats(tokenId)
+  {
+
+    return (
+      {
+        attack: Number(tokenId) % 102,
+        element: Number(tokenId) % 5,
+        speed: Number(tokenId) % 42,
+        crit: Number(tokenId) % 12
+      }
+    )
+  }
+
+
   app.get('/kitty/:id', (req, res) => {
-    var kittyStats = {
-      attack: 50,
-      speed: 20,
-      fire: 2
-    }
-    return res.status(200).send(kittyStats);
+    const {id} = req.params;
+    return res.status(200).send(getKittyStats(id));
   });
 
   app.post('/kitty/:id', (req, res) => {
-    var kittyStats = {
-      attack: 50,
-      speed: 20,
-      fire: 2
-    }
-    return res.status(200).send(kittyStats);
+    const {id} = req.params;
+    return res.status(200).send(getKittyStats(id));
   });
 
 
