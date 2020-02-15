2b4315151e71ca18d8f66e82383117c6d40d4767
Paul Gadi <paulgadi@gmail.com>
2020-02-15 11:35:09 -0700

    created damage computation

--- src/routes/kitty.ts
+++ src/routes/kitty.ts
@@ -4,6 +4,7 @@ export default (app) => {
   // element: Number(this.kitty.tokenId) % 5, -> should return 0 - 4
   // speed: Number(this.kitty.tokenId) % 42, -> should return a middle number
   // crit: Number(this.kitty.tokenId) % 12 -> should return a low number
+  const SPEED_CAP = 42;
 
   function getKittyStats(tokenId)
   {
@@ -13,18 +14,10 @@ export default (app) => {
         tokenId: tokenId,
         attack: Number(tokenId) % 102,
         element: Number(tokenId) % 5,
-        speed: Number(tokenId) % 42,
+        speed: Number(tokenId) % SPEED_CAP,
         crit: Number(tokenId) % 12
       }
     )
-  }
-
-  function getKittyDamage(kittyInfo)
-  {
-    return {
-      ...kittyInfo,
-      damage: Number(kittyInfo.tokenId)
-    }
   }
 
   function getKittiesKilled(bossId)
@@ -32,15 +25,53 @@ export default (app) => {
     return [20, 30, 5, 60, 80];
   }
 
-  function getBossInfo(bossId)
+  function getBossStats(bossId)
   {
-
-    // get boss stats
-    var bossStats = {
+    var bossStats =
+    {
       element: 4,
       type: 5,
       skinThickness: 100
-    }
+    };
+
+    return bossStats;
+  }
+
+  function computeDamage(kitty, bossStats)
+  {
+
+    // get elements
+    var bossElement = bossStats.element;
+    var kittyElement = kitty.element;
+    var kittyBaseDamage = kitty.attack;
+
+    // get element damage mulitplier
+    var elementMultiplier = computeElementMultiplier(bossElement, kittyElement);
+
+    // get total attack damage
+    var totalAttackDamage = kittyBaseDamage * elementMultiplier * (SPEED_CAP  - kitty.speed);
+
+    return totalAttackDamage;
+  }
+
+  function computeElementMultiplier(bossElement, kittyElement)
+  {
+    const elementTable =
+    [
+      [3, 3, 3, 3, 3],
+      [3, 3, 3, 3, 3],
+      [3, 3, 3, 3, 3],
+      [3, 3, 3, 3, 3],
+      [3, 3, 3, 3, 3]
+    ]
+
+    return elementTable[bossElement][kittyElement];
+  }
+
+  function getBossInfo(bossId)
+  {
+    // get boss stats
+    var bossStats = getBossStats(bossId);
 
     // get tokenIds of all kitties killed by boss
     var killList = getKittiesKilled(bossId);
@@ -48,8 +79,16 @@ export default (app) => {
     // get detailed data for each kitty
     var kittyInfo = killList.map(getKittyStats);
 
-    // compute damage by each Kitty
-    var ripKitties = kittyInfo.map(getKittyDamage);
+    console.log(bossStats);
+
+    // compute damage by each Kitty then add onto KittyInfo as ripKitties
+    var ripKitties = kittyInfo.map(kitty =>
+    {
+      return {
+        ...kitty,
+        damage: computeDamage(kitty, bossStats)
+      }
+    })
 
     return (
       {
